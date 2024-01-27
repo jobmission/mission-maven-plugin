6be123fac1449437493755bae8a0779594b96879
wzhang <contact@revengemission.com>
2024-01-27 10:37:32 +0800

    update

--- src/main/java/com/revengemission/plugins/maven/CheckLibraryVersionMojo.java
+++ src/main/java/com/revengemission/plugins/maven/CheckLibraryVersionMojo.java
@@ -56,9 +56,9 @@ public class CheckLibraryVersionMojo extends AbstractMojo {
                 getLog().info("The following libs have newer versions:");
                 libVersionList.forEach(libVersion -> {
                     if ("git".equalsIgnoreCase(libVersion.getRepositoryType())) {
-                        String url = String.format("https://api.github.com/repos/%s/%s/releases?per_page=10&page=1", libVersion.getOwner(), libVersion.getRepository());
+                        String releasesUrl = String.format("https://api.github.com/repos/%s/%s/releases?per_page=10&page=1", libVersion.getOwner(), libVersion.getRepository());
                         try {
-                            HttpRequest request = HttpRequest.newBuilder(new URI(url))
+                            HttpRequest request = HttpRequest.newBuilder(new URI(releasesUrl))
                                 .header("Accept", "application/json")
                                 .build();
 
@@ -69,7 +69,7 @@ public class CheckLibraryVersionMojo extends AbstractMojo {
                             List<GitReleaseModel> gitReleaseModelList = objectMapper.readValue(response, new TypeReference<>() {
                             });
 
-                            if (gitReleaseModelList != null && gitReleaseModelList.size() > 0) {
+                            if (gitReleaseModelList != null && !gitReleaseModelList.isEmpty()) {
                                 DefaultArtifactVersion oldVersion = new DefaultArtifactVersion(libVersion.getVersion());
                                 DefaultArtifactVersion latestVersion = oldVersion;
                                 for (int i = 0; i < gitReleaseModelList.size(); i++) {
@@ -85,11 +85,38 @@ public class CheckLibraryVersionMojo extends AbstractMojo {
                                     getLog().info(libVersion.getRepository() + " has newer version ... " + libVersion.getVersion() + " -> " + latestVersion);
                                 }
                             } else {
-                                getLog().warn(libVersion.getRepository() + " 未获取到版本记录 ");
+                                String tagsUrl = String.format("https://api.github.com/repos/%s/%s/tags?per_page=10&page=1", libVersion.getOwner(), libVersion.getRepository());
+                                HttpRequest requestTags = HttpRequest.newBuilder(new URI(tagsUrl))
+                                    .header("Accept", "application/json")
+                                    .build();
+
+                                String responseTags = HttpClient.newHttpClient()
+                                    .send(requestTags, HttpResponse.BodyHandlers.ofString())
+                                    .body();
+
+                                List<GitReleaseModel> gitReleaseModelListTags = objectMapper.readValue(responseTags, new TypeReference<>() {
+                                });
+
+                                if (!gitReleaseModelListTags.isEmpty()) {
+                                    DefaultArtifactVersion oldVersion = new DefaultArtifactVersion(libVersion.getVersion());
+                                    DefaultArtifactVersion latestVersion = oldVersion;
+                                    for (int i = 0; i < gitReleaseModelListTags.size(); i++) {
+                                        GitReleaseModel gitReleaseModel = gitReleaseModelListTags.get(i);
+                                        DefaultArtifactVersion gitVersion = new DefaultArtifactVersion(gitReleaseModel.getName());
+                                        if (gitVersion.compareTo(latestVersion) > 0) {
+                                            latestVersion = gitVersion;
+                                        }
+                                    }
+                                    if (latestVersion.compareTo(oldVersion) > 0) {
+                                        getLog().info(libVersion.getRepository() + " has newer version ... " + libVersion.getVersion() + " -> " + latestVersion);
+                                    }
+                                } else {
+                                    getLog().warn(libVersion.getRepository() + "(" + libVersion.getRepositoryType() + ") 未获取到版本记录 ");
+                                }
                             }
 
                         } catch (Exception e) {
-                            throw new RuntimeException(e);
+                            throw new RuntimeException(releasesUrl, e);
                         }
                     } else if ("maven".equalsIgnoreCase(libVersion.getRepositoryType())) {
                         String url = String.format("https://search.maven.org/solrsearch/select?q=g:%s+AND+a:%s&core=gav&rows=20&wt=json", libVersion.getOwner(), libVersion.getRepository());
@@ -118,14 +145,15 @@ public class CheckLibraryVersionMojo extends AbstractMojo {
                                     getLog().info(libVersion.getRepository() + " has newer version ... " + libVersion.getVersion() + " -> " + latestVersion);
                                 }
                             } else {
-                                getLog().warn(libVersion.getRepository() + " 未获取到版本记录 ");
+                                getLog().warn(libVersion.getRepository() + "(" + libVersion.getRepositoryType() + ") 未获取到版本记录 ");
                             }
 
                         } catch (Exception e) {
-                            throw new RuntimeException(e);
+                            throw new RuntimeException(url, e);
                         }
                     } else if ("npm".equalsIgnoreCase(libVersion.getRepositoryType())) {
-                        String url = String.format("https://registry.npmjs.org/-/v1/search?text=%s&size=5", libVersion.getRepository());
+                        /// String url = String.format("https://registry.npmjs.org/-/v1/search?text=%s&size=5", libVersion.getRepository());
+                        String url = String.format("https://registry.npmmirror.com/-/v1/search?text=%s&size=5", libVersion.getRepository());
                         try {
                             HttpRequest request = HttpRequest.newBuilder(new URI(url))
                                 .header("Accept", "application/json")
@@ -151,11 +179,11 @@ public class CheckLibraryVersionMojo extends AbstractMojo {
                                     getLog().info(libVersion.getRepository() + " has newer version ... " + libVersion.getVersion() + " -> " + latestVersion);
                                 }
                             } else {
-                                getLog().warn(libVersion.getRepository() + " 未获取到版本记录 ");
+                                getLog().warn(libVersion.getRepository() + "(" + libVersion.getRepositoryType() + ") 未获取到版本记录 ");
                             }
 
                         } catch (Exception e) {
-                            throw new RuntimeException(e);
+                            throw new RuntimeException(url, e);
                         }
                     } else if ("docker".equalsIgnoreCase(libVersion.getRepositoryType())) {
                         String url = String.format("https://hub.docker.com/v2/repositories/library/%s/tags?page_size=10", libVersion.getRepository());
@@ -184,11 +212,11 @@ public class CheckLibraryVersionMojo extends AbstractMojo {
                                     getLog().info(libVersion.getRepository() + " has newer version ... " + libVersion.getVersion() + " -> " + latestVersion);
                                 }
                             } else {
-                                getLog().warn(libVersion.getRepository() + " 未获取到版本记录 ");
+                                getLog().warn(libVersion.getRepository() + "(" + libVersion.getRepositoryType() + ") 未获取到版本记录 ");
                             }
 
                         } catch (Exception e) {
-                            throw new RuntimeException(e);
+                            throw new RuntimeException(url, e);
                         }
                     }
 
